ddbb91f44db02737fca49900146db7210036e805
Timur Khakkiev <khakkievtimur@yandex.ru>
2023-08-28 09:17:18 +0300

    add path resolver & vite path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+
+const pathResolver = (rPath: string) => path.resolve(__dirname, rPath);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': pathResolver('./src'),
+    },
+  },
   plugins: [react()],
 })
